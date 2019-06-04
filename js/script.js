4a05fe9aed3e68610a109ca5c69d34d93f01e65d
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-04 13:49:16 +0200

    Add js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+'use strict';
+(function(){ 
+  
+	var showModal = function(event){
+		event.preventDefault();
+		document.querySelector('#modal-overlay').classList.add('show');
+		document.querySelector('.modal').classList.add('show');
+	};
+  
+	var modalLinks = document.querySelectorAll('.show-modal');
+	
+	for(var i = 0; i < modalLinks.length; i++){
+		modalLinks[i].addEventListener('click', showModal);
+	}
+
+	var hideModal = function(event){
+		event.preventDefault();
+		document.querySelector('#modal-overlay').classList.remove('show');
+	};
+	
+	var closeButtons = document.querySelectorAll('.modal .close');
+	
+	for(var i = 0; i < closeButtons.length; i++){
+		closeButtons[i].addEventListener('click', hideModal);
+	}
+	
+	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+  
+	var modals = document.querySelectorAll('.modal');
+	
+	for(var i = 0; i < modals.length; i++){
+		modals[i].addEventListener('click', function(event){
+			event.stopPropagation();
+		});
+	}
+  
+  
+})(); 
