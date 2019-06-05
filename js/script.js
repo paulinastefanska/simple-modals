20b31b509077ec86695678415dfcc2e27c963107
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-05 08:50:04 +0200

    Add comment in js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 'use strict';
 (function(){ 
   
+ //open modals
 	var showModal = function(event){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.add('show');
 		document.querySelector('.modal').classList.add('show');
 	};
   
+ //modals links
 	var modalLinks = document.querySelectorAll('.show-modal');
 	
 	for(var i = 0; i < modalLinks.length; i++){
 		modalLinks[i].addEventListener('click', showModal);
 	}
 
+//close modals
 	var hideModal = function(event){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.remove('show');
@@ -24,8 +27,10 @@
 		closeButtons[i].addEventListener('click', hideModal);
 	}
 	
+//close modals - click in overlay
 	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
   
+//stop propagation  
 	var modals = document.querySelectorAll('.modal');
 	
 	for(var i = 0; i < modals.length; i++){
