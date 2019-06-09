3f15b2ffb64188ab86497232dd48b0b39e0cabf2
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-09 13:23:17 +0200

    Fix open modals in js

--- js/script.js
+++ js/script.js
@@ -1,44 +1,50 @@
 'use strict';
-(function(){ 
+(function(){  
   
- //open modals
+//open modals
+  var modals = document.querySelectorAll('.modal');
+  
 	var showModal = function(event){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.add('show');
+    
+    for(var i = 0; i < modals.length; i++)
+    {
+      modals[i].classList.remove('show');
+    }
+    
+    var name = this.getAttribute('href');
+    
+    document.querySelector(name).classList.add('show');
 	};
-  
- //modals links
 	var modalLinks = document.querySelectorAll('.show-modal');
 	
 	for(var i = 0; i < modalLinks.length; i++){
-		modalLinks[i].addEventListener('click', function(event){
-			
-		});
+		modalLinks[i].addEventListener('click', showModal);
 	}
 
 //close modals
-	var hideModal = function(event){
-		event.preventDefault();
-		document.querySelector('#modal-overlay').classList.remove('show');
-	};
+var hideModal = function(event){
+	event.preventDefault();
+	document.querySelector('#modal-overlay').classList.remove('show');
+};
 	
-	var closeButtons = document.querySelectorAll('.modal .close');
-	
-	for(var j = 0; j < closeButtons.length; j++){
-		closeButtons[j].addEventListener('click', hideModal);
-	};
+var closeButtons = document.querySelectorAll('.modal .close');
+
+for(var j = 0; j < closeButtons.length; j++){
+	closeButtons[j].addEventListener('click', hideModal);
+};
 	
 //close modals - click in overlay
-	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
   
 //stop propagation  
-	var modals = document.querySelectorAll('.modal');
-	
-	for(var k = 0; k < modals.length; k++){
-		modals[k].addEventListener('click', function(event){
-			event.stopPropagation();
-		});
-	}
-  
-  
+var modals = document.querySelectorAll('.modal');
+
+for(var k = 0; k < modals.length; k++){
+	modals[k].addEventListener('click', function(event){
+		event.stopPropagation();
+	});
+}
+ 
 })(); 
